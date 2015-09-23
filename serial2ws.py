db6d505c781e12766e2eef9d91784957a3fe1a71
flashbac <flashbac0@gmail.com>
2015-09-23 17:47:46 +0200

    extend default port

--- serial2ws.py
+++ serial2ws.py
@@ -556,10 +556,44 @@ class McuComponent(ApplicationSession):
     def publish_test(self, topic, args):
         self.publish(topic, args)
 
+import sys, traceback
+def print_exc_plus():
+    """
+    Print the usual traceback information, followed by a listing of all the
+    local variables in each frame.
+    """
+    tb = sys.exc_info()[2]
+    while 1:
+        if not tb.tb_next:
+            break
+        tb = tb.tb_next
+    stack = []
+    f = tb.tb_frame
+    while f:
+        stack.append(f)
+        f = f.f_back
+    stack.reverse()
+    traceback.print_exc()
+    print "Locals by frame, innermost last"
+    for frame in stack:
+        print
+        print "Frame %s in %s at line %s" % (frame.f_code.co_name,
+                                             frame.f_code.co_filename,
+                                             frame.f_lineno)
+        for key, value in frame.f_locals.items():
+            print "\t%20s = " % key,
+            #We have to be careful not to cause a new error in our error
+            #printer! Calling str() on an unknown object could cause an
+            #error we don't want.
+            try:
+                print value
+            except:
+                print "<ERROR WHILE PRINTING VALUE>"
+
 
 if __name__ == '__main__':
 
-    import sys
+
     import argparse
     # log.setLevel(logging.DEBUG)
     # parse command line arguments
@@ -626,8 +660,14 @@ if __name__ == '__main__':
 
     router = args.router or u'ws://127.0.0.1:8080/ws/'
 
-    runner = ApplicationRunner(router, u"me_gsv6",
-                               extra={'port': args.port, 'baudrate': args.baudrate, 'debug': True})
+    try:
+        runner = ApplicationRunner(router, u"me_gsv6",
+                                   extra={'port': args.port, 'baudrate': args.baudrate, 'debug': True})
+    except:
+        print("error")
+        traceback.print_exc()
+        print_exc_plus()
+
     # extra={'port': args.port, 'baudrate': args.baudrate, 'debug': args.debug})
 
     # start the component and the Twisted reactor ..
